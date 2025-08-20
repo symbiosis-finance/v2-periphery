fad817388a623f43f6e39b8481963d0211d697a6
Nikolay Bryskin <nikicat@users.noreply.github.com>
2025-08-20 14:22:49 +0300

    fix UniswapV2Pair init code hash

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'75adb48f857694416761aea5396781eab38703a3cb4992e5d28e3c3db439ec5e' // init code hash
             ))));
     }
 
